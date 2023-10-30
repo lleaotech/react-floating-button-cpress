61ad62caa3f69cc59b457ac59df483179c3fa18a
Lucas Leao <55324755+lleaonet@users.noreply.github.com>
2023-10-30 10:50:03 +0000

    Initial commit

---
+++ FloatingButton/index.js
@@ -0,0 +1,171 @@
+/**
+ * @param {number} size             the size  of the buttons.
+ * @param {boolean} top             specify if the button should be on the top if false the
+ * button will be at the bottom.
+ * @param {boolean} right           specify if the button should be on the right if false the
+ * button will be at the left.
+ * @param {string} color            the backgroundColor for the main button
+ * @children should be an Item component with params :
+ *  @param {string} imgSrc          the icon to use on given button
+ *  @param {function} onClick       the callback function call onClick
+ *  @param {string} backgroundColor            the backgroundColor for the Item
+ */
+
+import React, { useState, useEffect, useRef } from "react";
+import { Container, Floating, Item } from "./styles";
+import { PoseGroup } from "react-pose";
+import PropTypes from "prop-types";
+import MenuToggle from "./hamburger";
+
+const rotations = {
+  "3": [
+    [(3 * Math.PI) / 2, Math.PI],
+    [0, Math.PI / 2],
+  ],
+  "6": [
+    [Math.PI, Math.PI],
+    [0, 0],
+  ],
+};
+
+function FloatingButton({
+  backgroundColor,
+  color,
+  size,
+  top,
+  right,
+  icon,
+  children,
+}) {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef(null);
+
+  let number = React.Children.count(children);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  });
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setExpanded(false);
+    }
+  };
+
+  function getAngle(i) {
+    const angle =
+      number <= 3 ? Math.PI / 2 : number <= 6 ? Math.PI : 2 * Math.PI;
+    const rotate =
+      rotations[number <= 3 ? "3" : "6"][Number(top)][Number(right)];
+    return {
+      angle:
+        rotate +
+        (number <= 6 ? (i * angle) / (number - 1) : (i * angle) / number),
+      distance:
+        number <= 6
+          ? size / Math.sin(angle / (number - 1)) + size / 2
+          : size / Math.sin(angle / number) + size / 2,
+    };
+  }
+
+  return (
+    <Floating
+      onClick={() => {
+        setExpanded(!expanded);
+      }}
+      top={top}
+      right={right}
+      pose={expanded ? "open" : "closed"}
+      number={number}
+      distance={getAngle(0).distance}
+      ref={ref}
+    >
+      <Container
+        size={size}
+        style={{ backgroundColor: `${backgroundColor || "none"}` }}
+      >
+        {icon ? (
+          <img
+            src={icon}
+            style={{
+              height: size / 2,
+              width: size / 2,
+            }}
+            alt={"icon"}
+          />
+        ) : (
+          <MenuToggle expanded={expanded} color={color} size={size} />
+        )}
+      </Container>
+      {number === 1 ? (
+        <Item
+          key={0}
+          i={getAngle(0).angle}
+          size={size}
+          distance={getAngle(0).distance}
+          style={{
+            backgroundColor: children.props.backgroundColor,
+          }}
+          onClick={() => children.props.onClick()}
+        >
+          <img
+            src={children.props.imgSrc}
+            style={{
+              height: size / 2,
+              width: size / 2,
+              fill: "white",
+            }}
+            alt={"icon"}
+          />
+        </Item>
+      ) : (
+        <PoseGroup>
+          {expanded &&
+            [...Array(number)].map((x, i) => (
+              <Item
+                key={i}
+                i={getAngle(i).angle}
+                size={size}
+                distance={getAngle(i).distance}
+                style={{
+                  backgroundColor: children[i].props.backgroundColor,
+                }}
+                onClick={() => children[i].props.onClick()}
+              >
+                <img
+                  src={children[i].props.imgSrc}
+                  style={{ height: size / 2, width: size / 2 }}
+                  alt={`icon-${i}`}
+                />
+              </Item>
+            ))}
+        </PoseGroup>
+      )}
+    </Floating>
+  );
+}
+
+FloatingButton.defaultProps = {
+  color: "#dbdbdb",
+  backgroundColor: "#8f1d30",
+  size: 60,
+  top: false,
+  right: true,
+  children: {},
+};
+
+FloatingButton.propTypes = {
+  color: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  size: PropTypes.number,
+  top: PropTypes.bool,
+  right: PropTypes.bool,
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+};
+
+export default FloatingButton;
+export { FloatingButton };
+export { Item };
---
+++ README.md
@@ -0,0 +1,84 @@
+<h1 align="center">React Awesome Floating Button</h1>
+
+
+<br>
+
+
+### <h1 align="center">An Awesome & Unique Customizable Floating Button</h1>
+
+<p align="center">
+    Powered by The WuuD Team
+</p>
+
+## Build & Run
+
+
+
+#### Install
+
+With npm
+
+```bsh
+npm install react-floating-button
+```
+
+With yarn
+
+```bsh
+yarn add react-floating-button
+```
+
+#### Usage
+
+```javascript
+import { FloatingButton, Item } from "react-floating-button";
+import downloadIcon from "./assets/DOWNLOAD.svg";
+import forwardIcon from "./assets/FORWARD.svg";
+
+<FloatingButton>
+  <Item
+    imgSrc={downloadIcon}
+    onClick={() => {
+      console.log("callback function here");
+    }}
+  />
+  <Item
+    imgSrc={forwardIcon}
+    onClick={() => {
+      console.log("callback function here");
+    }}
+  />
+</FloatingButton>;
+```
+
+## Options
+
+### FloatingButton
+
+| Property        | Description                                                                          | Type    | Default   |
+| --------------- | ------------------------------------------------------------------------------------ | ------- | --------- |
+| right           | Specify if the button should be on the right if false the button will be at the left | boolean | `true`    |
+| top             | Specify if the button should be on the right if false the button will be at the left | boolean | `false`   |
+| size            | The size used for the buttons                                                        | number  | `60`      |
+| backgroundColor | The backgroundColor for the main button                                              | string  | `#8f1d30` |
+| color           | The color for the burger icon                                                        | string  | `#dbdbdb` |
+
+### Item
+
+| Property        | Description                      | Type   | Default   |
+| --------------- | -------------------------------- | ------ | --------- |
+| imgSrc          | The icon to use on given button  | string | -         |
+| backgroundColor | The backgroundColor for the Item | string | `#dbdbdb` |
+| onClick         | Called when an item is clicked   | func   | -         |
+
+## Attribution
+
+- React Pose powered by [Popmotion](https://popmotion.io/pose/)
+- Built & Developed from [React JS](https://reactjs.org/)
+
+
+## License
+
+The code is available under the [MIT] license.
+
+
